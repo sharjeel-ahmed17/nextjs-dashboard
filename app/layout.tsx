79d6b1cece61647232eccae65d2afadcc9dbc5fc
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2024-11-28 01:38:58 +0500

    database_connecting

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-       <body className={`${inter.className} antialiased`}>{children}</body>
+       {/* <body className={`${inter.className} antialiased`}>{children}</body> */}
+       <body className="">{children}</body>
     </html>
   );
 }
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,8 +1,21 @@
+// import localFont from "next/font/local";
 import { Inter, Lusitana } from 'next/font/google';
+export const inter = Inter({ subsets: ['latin'], display: 'swap', adjustFontFallback: false});
  
-export const inter = Inter({ subsets: ['latin'] });
- 
-export const lusitana = Lusitana({
-  weight: ['400', '700'],
-  subsets: ['latin'],
-}); 
+export const lusitana = Lusitana({weight: ['400', '700'],subsets: ['latin'],adjustFontFallback: false}); 
+// export const lusitana = Lusitana({
+//   subsets: ["latin"],
+//   variable: "--font-lusitana",
+//   display: "swap",
+//   adjustFontFallback: false,
+// });
+// export const lusitana = localFont({
+//   src: "/fonts/Inter_28pt-Regular.ttf",
+//   variable: "--font-lusitana",
+//   weight: "100 900",
+// });
+// export const inter = localFont({
+//   src: "/fonts/Lusitana-Regular.ttf",
+//   variable: "--font-inter",
+//   weight: "100 900",
+// });
